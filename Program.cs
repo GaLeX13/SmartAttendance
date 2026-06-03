8a5fe0beeabc276542e71e889b9e9b96c787c172
GaLeX13 <gheorghea541@gmail.com>
2026-06-04 02:22:46 +0300

    Clean UI language and remove unused project code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using SmartAttendance.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 
@@ -14,14 +12,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     )
 );
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -35,6 +30,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
