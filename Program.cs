c8300bfb2756310bdb98191539790e6320bcde58
GaLeX13 <gheorghea541@gmail.com>
2026-06-05 06:01:48 +0300

    Apply database migrations on application startup

--- Program.cs
+++ Program.cs
@@ -24,13 +24,19 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
 app.UseSession();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
